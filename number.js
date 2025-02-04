75d3b84a49a3a733aca7c97159c289d693fb20bd
Md Raduan Ahamed <raduanahmed49591@gmail.com>
2025-02-04 22:14:38 +0600

    explore some numbering lets a int float typeof parseInt parseFloat tofixed

---
+++ number.js
@@ -0,0 +1,27 @@
+var OrangePrice = 20;
+var chocolate = 0.5;
+// var applePrice ="25";
+// var applePrice = parseInt("25");
+var applePrice = parseInt("25.2");
+var applePrice = parseFloat("25.2");
+
+console.log(OrangePrice);
+console.log(OrangePrice + chocolate);
+console.log(OrangePrice + applePrice);
+console.log (typeof OrangePrice);
+console.log (typeof applePrice);
+console.log (applePrice);
+
+
+var first =0.1;
+var second =0.2;
+var total=first + second;
+console.log(total)
+console.log(total.toFixed(4))
+
+
+const num =50/0;
+console.log(num)
+const num = -50/0;
+console.log(num)
+
