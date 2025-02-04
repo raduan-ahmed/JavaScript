5a639414d0c799de16c431ce6a18858529be06d0
Md Raduan Ahamed <raduanahmed49591@gmail.com>
2025-02-04 21:50:17 +0600

    explore some naming convention and  rules of declairation variable name

---
+++ naming.js
@@ -0,0 +1,26 @@
+// 1. No keyword in variable name
+var false =46;
+var var =50;
+
+// 2. No space or gap in variable name
+var is happy =false;
+var ishappy= true;
+
+// 3. no quote
+var "variable" ="anything";
+
+// 4. can not start with a number . but number other than the first letter is allowed
+var money=45;
+var money2=55;
+var 8money=555;
+
+// 5. name is case sensitive
+address 
+Address 
+ADDRESS
+
+// 6. how to write a long variable name
+var my current home address ="Mymensing";
+var my current_home_address ="Mymensing"; 
+var myCurrentHomeAddress ="Mymensing";
+var my current home address ="Mymensing";
