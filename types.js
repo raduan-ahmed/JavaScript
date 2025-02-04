753310be119cc3b1ecae2e2384076dcf476d1250
Md Raduan Ahamed <raduanahmed49591@gmail.com>
2025-02-04 21:36:17 +0600

    explore the type of variable boolean string

---
+++ types.js
@@ -0,0 +1,18 @@
+// this is string type variable that can define with the "..."
+var name="Raduan";
+console.log(name);
+
+var country="Bangladesh";
+console.log(country);
+
+
+// this is boolean type variable (true,false)
+var isRich=true;
+console.log(isRich);
+
+var isRich=false;
+console.log(isRich);
+
+// which type of variable checkout this
+console.log(typeof isRich);
+console.log(typeof country);
