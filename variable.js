4e6ceb4df81ba38163a908a8b272f2db03e7d27a
Md Raduan Ahamed <raduanahmed49591@gmail.com>
2025-02-04 21:26:07 +0600

    explore variable var

---
+++ variable.js
@@ -0,0 +1,16 @@
+/**
+ * VARIABLE==>vary + able
+ * ****************************
+ * 5 things to declare a variable
+ * 
+ * 
+ */
+
+var weight = 38;
+var price = 38;
+var year = 1995;
+var money = 2005;
+
+var age;
+console.log(age);
+console.log(price);
