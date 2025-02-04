6eb80aaa6ba6e258536d13216f15688bd9603d5e
Md Raduan Ahamed <raduanahmed49591@gmail.com>
2025-02-04 20:57:38 +0600

    added some important note for basic math

--- first.js
+++ first.js
@@ -1,1 +1,17 @@
-console.log(55)
+/**
+ * ***************************************
+ *       Math needed for Programming
+ * ***************************************
+ * Number: +ve, -ve, 0 [50,-9,0]
+ * integer: 2,88,[odd,even]:2,4,897
+ * float: 5.6, 5.2
+ * operation: +,-,*,/
+ * More operation: %, [ ]
+ * percentage: 5%, 50 taka er 10% koto?
+ * sum: 14,18,16
+ * Avg: 14,18,16
+ * Max/Min: 14,18,16
+ * series:
+ * Area: Triangle
+ * 
+ */
