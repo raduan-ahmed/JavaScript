c7119db09d4076f78039b2cf95c5e5c21e550730
Md Raduan Ahamed <raduanahmed49591@gmail.com>
2025-02-05 21:43:30 +0600

    explore comparison in javascript and on going if-else concept

---
+++ comparison.js
@@ -0,0 +1,30 @@
+// console.log(5>10);
+// console.log(5<10);
+// console.log(5==10);
+// console.log(10==10);
+// console.log(15>=10);
+// console.log(5>=10);
+// console.log(10 <=10);
+// ---------------------------------------------
+
+
+var peyaraPrice=100;
+var applePrice=250;
+
+// console.log(peyaraPrice > applePrice);
+console.log(applePrice < peyaraPrice);
+
+
+/**
+ * ==
+ * ===
+ * 
+ */
+
+// console.log(10==10);
+// console.log(10 =='10');
+// console.log(10 ==='10');
+
+
+// console.log(10 != '10' );
+console.log(10 !== '10');
