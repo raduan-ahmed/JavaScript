ca05f73daad3ba3ce7e1905a5134d9d7e713723b
Md Raduan Ahamed <raduanahmed49591@gmail.com>
2025-02-09 11:22:11 +0600

    explore logical not operation

---
+++ shortcut.js
@@ -0,0 +1,16 @@
+const isleader = true;
+// if (isleader){
+//     console.log('leader asche ! chair charo');
+// }
+// else{
+//     console.log('vitore dukte dibi na');
+// }
+
+const isPassed = false;
+// if (isPassed === false){
+if (! isPassed){
+    console.log('thsi  not passed')
+}
+else{
+    console.log('tumi onk valo')
+}
