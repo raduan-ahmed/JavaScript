3fa42bc7d35efb326d4a79321f76d2d80213c128
Md Raduan Ahamed <raduanahmed49591@gmail.com>
2025-02-09 10:51:05 +0600

    explore nested if else condition

---
+++ nested.js
@@ -0,0 +1,17 @@
+const money =30 ;
+if (money >400){
+    console.log('Bro yyou are rich');
+}
+else{
+    if(money >100){
+        console.log('tui borolok o na gorib o na')
+    }
+    else{
+        if(money > 0){
+            console.log('dosto ------------')
+        }
+        else{
+            console.log('nai-------------')
+        }
+    }
+}
