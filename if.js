7ce9bdd9a2920b01f8649d2b4740a7745f4cc83f
Md Raduan Ahamed <raduanahmed49591@gmail.com>
2025-02-09 10:16:02 +0600

    explore multiple condition logical operation

---
+++ if.js
@@ -0,0 +1,28 @@
+/**
+ * if (condition){
+ * 
+ * 
+ * }
+ */
+
+if(3>10){
+    console.log('This is Right');
+
+}
+
+
+// var birianiPrice = 200;
+
+// if (birianiPrice <500){
+//     console.log("Give me some biriani")
+// }
+
+
+var birianiPrice = 900;
+
+if (birianiPrice <500){
+    console.log("Give me some biriani")
+}
+else{
+    console.log("I will go home")
+}
---
+++ multiple.js
@@ -0,0 +1,37 @@
+const salary = 25000;
+const isBCS = true;
+const height = 61;
+hscar = false;
+// if (salary > 20000 && height >66){
+//     console.log('su--------------patro');
+// }
+
+// else{
+//     console.log('omno---------patro')
+// }
+
+if (salary > 20000 || height >66){
+    console.log('su--------------patro');
+}
+
+else{
+    console.log('omno---------patro')
+}
+
+
+
+// more and more condition
+// if (salary > 20000 || height >66 || isBCS == true){
+//     console.log('su--------------patro');
+// }
+
+// else{
+//     console.log('omno---------patro')
+// }
+
+
+
+// -------------complex condition-------------------
+// if ((salary>25000 && hscar ==true ) || isBCS == true){
+//     console.log('tomar 14 gosti raji')
+// }
